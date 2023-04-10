9e62a5456c02523a791ce21a252dc2c6ddd4a064
prh <1875377706@qq.com>
2023-04-10 11:03:48 +0800

    add

--- src/hooks/useRandom.ts
+++ src/hooks/useRandom.ts
@@ -2,9 +2,12 @@ import { ref } from "vue";
 
 export default (length: number) => {
  const result = ref('')
- const characters = 'abcdefghijklmnopqrstuvwxyz1234567890';
- for (let i = 0; i < length; i++) {
-  result.value += characters.charAt(Math.floor(Math.random() * characters.length));
- }
+ const generateRandom = () => {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < length; i++) {
+    result.value += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+ };
+ generateRandom()
  return { result }
 }
